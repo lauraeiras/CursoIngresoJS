e88d02ca2c98881043f1fd2432d05334a7990f8a
laly eiras <lalyss@hotmail.es>
2020-07-08 16:47:36 -0300

    tomo dato x prompt()

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,8 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var nombre;                            // declaro mi variable
+	nombre = prompt("Ingrese su nombre");  // prompt(toma datos de usuario),hago tantos prompt segun datos sean necesarios
+	alert("Su nombre es : "+nombre);      // alert(muestra msj en pantalla)el espacio va dentro de las ""
 }
 

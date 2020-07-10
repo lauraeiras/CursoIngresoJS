5ed5e2af56763b2909233d0dc902667cd98f2c4c
laly eiras <lalyss@hotmail.es>
2020-07-10 13:06:38 -0300

    Porcentaje

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,13 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	var resultado;
 
-	alert("ok");
+	sueldo=txtIdSueldo.value;
+	resultado=parseInt(sueldo)*1.10;
+	txtIdResultado.value=resultado;
+
+
+	
 }

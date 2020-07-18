063e8c5c43b915b2e1cda42e46ec5ab908596439
laly eiras <lalyss@hotmail.es>
2020-07-18 13:48:43 -0300

    Repaso Sabado

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,15 +1,12 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
 	var numero;
-
-	numero = parseInt(numero);
-
-	if( numero >= 1 && numero <= 10)
-		{
-			alert(numero);
-
-		}
+	//Genero el número RANDOM entre 1 y 10 
+	//Usar Math.random, cada vez que lo ejecuto me tira un numero random
+	console.log(Math.random()*10); //Al multiplicarlo por 10, tengo un número entre 0 y 9.99
+	/*Para redondear .floor
+	Math.floor(Math.random()*10)+1;
+	Math.floor(Math.random()*(max-min)+min;*/
 		
 
 }//FIN DE LA FUNCIÓN

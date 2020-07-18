7ada639f13fee6cd5db15fdb3722a95eb1ba9c9d
laly eiras <lalyss@hotmail.es>
2020-07-18 16:38:57 -0300

    Numero Random 9 y 10

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,9 +1,11 @@
 function mostrar()
 {
 	var numero;
+
+	numero = Math.floor(Math.random()*11);
 	//Genero el número RANDOM entre 1 y 10 
 	//Usar Math.random, cada vez que lo ejecuto me tira un numero random
-	console.log(Math.random()*10); //Al multiplicarlo por 10, tengo un número entre 0 y 9.99
+	console.log(numero); //Al multiplicarlo por 10, tengo un número entre 0 y 9.99
 	/*Para redondear .floor
 	Math.floor(Math.random()*10)+1;
 	Math.floor(Math.random()*(max-min)+min;*/
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,37 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+
+	
+
+	var nota;
+
+	nota = Math.floor(Math.random()*11);
+	console.log(nota);
+
+	if(nota == 9 || nota == 10  )
+	{
+		alert("EXCELENTE");
+
+	}
+	else
+	{  
+
+	if(nota >= 4)
+		{
+			alert("APROBÓ");
+		}
+		else
+			{
+				//if(nota < 4)// no es necesario
+
+				//{
+					alert("Vamos,la proxima se puede");
+				//}
+
+			}
+	 
+	}
+	
 
 }//FIN DE LA FUNCIÓN

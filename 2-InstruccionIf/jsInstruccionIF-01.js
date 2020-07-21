0023d2954e31a174b6d0a415c739a9928fa98b68
laly eiras <lalyss@hotmail.es>
2020-07-21 09:01:40 -0300

    Construccion if

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -5,11 +5,13 @@ function mostrar()
 
 	edad = txtIdEdad.value;
 
-	if ( edad == 15 )
-	{
+	if( edad == 15 ){//si la condicion es true ejec el codigo
+
 		alert("NiÑa Bonita");
 
-	}
+	}/*else{//si if es false se ejecuta este codigo
+
+	}*/
 	
 	
 

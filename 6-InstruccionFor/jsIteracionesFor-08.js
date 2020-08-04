474b805456d7c105ec6dd1bec16d2f096b82a560
laly eiras <lalyss@hotmail.es>
2020-08-04 16:38:50 -0300

    iteraciones for

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,7 +1,22 @@
-function mostrar()
-{
+function mostrar() {
+  var numero;
+  var contador = 1;
+  var contDivisores = 0;
 
+  do {
+    numero = prompt("Ingrese numero");
+    numero = parseInt(numero);
+  } while (isNaN(numero) || numero < 1);
 
-	
-
-}//FIN DE LA FUNCIÓN
+  for (contador = 1; contador <= numero; contador++) {
+    if (numero % contador == 0) {
+      console.log(contador);
+      contDivisores++;
+    }
+  }
+  if (contDivisores == 2) {
+    console.log(numero + " Es numero primo ");
+  } else {
+    console.log(numero + " No es numero primo");
+  }
+} //FIN DE LA FUNCIÓN
